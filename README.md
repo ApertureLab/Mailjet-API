35324c7e0ed296d620e19d9a96a79ff917872079
Narno <arnaud@ligny.org>
2013-06-28 17:32:21 +0200

    First usable release.

--- README.md
+++ README.md
@@ -12,16 +12,16 @@ How to use
 use ZendService\Mailjet\Mailjet as Mailjet;
 
 try {
-    $mj = New Mailjet('key', 'secret_key');
+    $mailjet = New Mailjet('key', 'secret_key');
     // user infos
-    $userInfos = $mj->userGetInfos();
-    echo $userInfos->firstname;
+    $userInfos = $mailjet->user->infos();
+    if ($userInfos->status == 'OK') {
+        echo $userInfos->infos->username;
+    }
 } catch (Exception $e) {
     echo $e->getMessage();
 }
 ```
-
-*Work in progress...*
 
 Mailjet API documentation: http://www.mailjet.com/docs/api
 
@@ -31,7 +31,7 @@ License
 
 _ZendService_Mailjet_ is released under the terms of the [MIT license](http://opensource.org/licenses/MIT).
 
-Copyright (c) 2012 Arnaud Ligny
+Copyright (c) 2012-2013 Arnaud Ligny
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- library/ZendService/Mailjet/Mailjet.php
+++ library/ZendService/Mailjet/Mailjet.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * @package Zend_Service
+ * ZendService\Mailjet
+ *
+ * @link      https://github.com/Narno/ZendService_Mailjet
+ * @copyright Copyright (c) 2012-2013 Arnaud Ligny
+ * @license   http://opensource.org/licenses/MIT MIT license
+ * @package   Zend_Service
  */
 
 namespace ZendService\Mailjet;
 
-use Zend\Http\Client as HttpClient;
-use Zend\Http\Request as HttpRequest;
+use Zend\Http;
 
 /**
  * @category   Zend
@@ -16,18 +20,23 @@ use Zend\Http\Request as HttpRequest;
 class Mailjet
 {
     /**
-     * Your Mailjet API key
+     * Mailjet API key
      *
      * @var string
      */
     protected $apiKey;
 
     /**
-     * Your Mailjet API secret key
+     * Mailjet API secret key
      *
      * @var string
      */
     protected $apiSecretKey;
+    
+    /**
+     * Output format
+     */
+    const OUTPUT_FORMAT = 'json';
 
     /**
      * Options
@@ -38,7 +47,6 @@ class Mailjet
         'uri'      => '{{protocol}}://api.mailjet.com/{{version}}/',
         'protocol' => 'http',
         'version'  => 0.1,
-        'output'   => 'json',
     );
 
     /**
@@ -47,37 +55,153 @@ class Mailjet
     protected $httpClient = null;
 
     /**
+     * Current method category (for method proxying)
+     *
+     * @var string
+     */
+    protected $methodCategory;
+
+    /**
+     * Categories of API methods
+     *
+     * @var array
+     */
+    protected $methodCategories = array(
+        'api',
+        'user',
+        'message',
+        'contact',
+        'lists',
+        'report',
+        'help',
+    );
+
+    /**
      * Performs object initializations
      *
-     * @param   string $apiKey
-     * @param   string $apiSecretKey
+     * @param string $apiKey
+     * @param string $apiSecretKey
+     * @param null|Http\Client $httpClient
      */
-    public function __construct($apiKey, $apiSecretKey, HttpClient $httpClient = null)
+    public function __construct($apiKey, $apiSecretKey, Http\Client $httpClient = null)
     {
         $this->apiKey       = (string) $apiKey;
         $this->apiSecretKey = (string) $apiSecretKey;
-        $this->setHttpClient($httpClient ?: new HttpClient)
+        $this->setHttpClient($httpClient ?: new Http\Client)
             ->setAuth($apiKey, $apiSecretKey);
     }
 
     /**
-     * @param HttpClient $httpClient
-     * @return HttpClient
+     * Proxy service methods category
+     *
+     * @param  string $category
+     * @return self
+     * @throws Exception If method not in method categories list
      */
-    public function setHttpClient(HttpClient $httpClient)
+    public function __get($category)
+    {
+        $category = strtolower($category);
+        if (!in_array($category, $this->methodCategories)) {
+            throw new Exception\RuntimeException(
+                'Invalid method category "' . $category . '"'
+            );
+        }
+        $this->methodCategory = $category;
+        return $this;
+    }
+
+    /**
+     * Method overloading
+     *
+     * @param  string $method
+     * @param  array $params
+     * @return mixed
+     * @throws Exception\RuntimeException if unable to find method
+     */
+    public function __call($method, $params)
+    {
+        $method = ucfirst($method);
+
+        /**
+         * If method category is not setted
+         */
+        if (empty($this->methodCategory)) {
+            throw new Exception\RuntimeException(
+                'Invalid method "' . $method . '"'
+            );
+        }
+
+        /**
+         * Build Mailjet method name: category + method
+         */
+        $method = $this->methodCategory . $method;
+
+        /**
+         * If local method exist, call it
+         */
+        if (method_exists($this, $method)) {
+            return call_user_func_array(array($this, $method), $params);
+        }
+        /**
+         * Else request API directly
+         */
+        else {
+            if ($method != 'helpMethod') {
+                $helpMethod = $this->helpMethod($method);
+                if ($helpMethod !== NULL && $helpMethod->status == 'OK') {
+                    $methodRequestType = strtoupper($helpMethod->method->request_type);
+                    if ($methodRequestType == 'GET') {
+                        return $this->requestGet($method, $params);
+                    }
+                    elseif ($methodRequestType == 'POST') {
+                        return $this->requestPost($method, $params);
+                    }
+                    else {
+                        throw new Exception\RuntimeException(
+                            'Invalid HTTP method "' . $methodRequestType . '"'
+                        );
+                    }
+                }
+                else {
+                    throw new Exception\RuntimeException(
+                        'Invalid method "' . $method . '"'
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * Set HTTP client
+     *
+     * @param Http\Client $httpClient
+     * @return Http\Client
+     */
+    public function setHttpClient(Http\Client $httpClient)
     {
         return $this->httpClient = $httpClient;
     }
 
     /**
-     * @return HttpClient
+     * Get HTTP client
+     *
+     * @return Http\Client
      */
     public function getHttpClient()
     {
         return $this->httpClient;
     }
 
-    public function execute($apiMethod, array $params = array(), $method = 'GET')
+    /**
+     * Perform an HTTP GET or POST request
+     * (GET by default)
+     *
+     * @param  string API method
+     * @param  array parameters
+     * @param  string HTTP method ('GET' or 'POST')
+     * @return object Response
+     */
+    public function request($apiMethod, array $params = array(), $method = 'GET')
     {
         // ie: api.mailjet.com/0.1/methodFunction?option=value
 
@@ -87,115 +211,69 @@ class Mailjet
             '{{version}}'  => $this->options['version'],
         ));
 
-        $params = array_merge($params, array('output' => $this->options['output']));
+        $params = array_merge($params, array('output' => self::OUTPUT_FORMAT));
 
-        $request = new HttpRequest;
+        $request = new Http\Request;
         $request->setUri($uri . $apiMethod);
 
         if (strtoupper($method) == 'GET') {
-            $request->setMethod(HttpRequest::METHOD_GET);
+            $request->setMethod(Http\Request::METHOD_GET);
             $request->getQuery()->fromArray($params);
-            $response = $this->httpClient
-                ->setMethod(HttpRequest::METHOD_GET)->send($request);
+            $response = $this->getHttpClient()
+                ->setMethod(Http\Request::METHOD_GET)->send($request);
         }
         elseif (strtoupper($method) == 'POST') {
-            $request->setMethod(HttpRequest::METHOD_POST);
+            $request->setMethod(Http\Request::METHOD_POST);
             $request->getPost()->fromArray($params);
-            $response = $this->httpClient
-                ->setMethod(HttpRequest::METHOD_POST)->send($request);
+            $response = $this->getHttpClient()
+                ->setMethod(Http\Request::METHOD_POST)->send($request);
         }
 
         if ($response->isServerError() || $response->isClientError()) {
-            throw new Exception\RuntimeException('An error occurred sending request. Status code: '
-                                                 . $response->getStatusCode());
+            throw new Exception\RuntimeException('An error occurred sending request. Status code: ' . $response->getStatusCode());
         }
 
-        return $response->getContent();
+        return json_decode($response->getBody());
     }
 
     /**
-     * Adds a contact to a list
+     * Perform an HTTP GET request
+     *
+     * @param string API method
+     * @param array parameters
+     * @return object Response
      */
-    public function listAddcontact($email, $listId, $force = true)
+    public function requestGet($apiMethod, array $params = array())
     {
-        static $apiMethod = 'listsAddcontact';
+        return $this->request($apiMethod, $params, 'GET');
+    }
 
-        $response = $this->execute(
+    /**
+     * Perform an HTTP POST request
+     *
+     * @param string API method
+     * @param array parameters
+     * @return object Response
+     */
+    public function requestPost($apiMethod, array $params = array())
+    {
+        return $this->request($apiMethod, $params, 'POST');
+    }
+
+    /**
+     * Get description of a method
+     *
+     * @param string API method
+     */
+    public function helpMethod($name)
+    {
+        static $apiMethod = 'HelpMethod';
+        $response = $this->requestGet(
             $apiMethod,
             array(
-                'contact' => $email,
-                'id'      => $listId,
-                'force'   => $force,
-            ),
-            'POST'
+                'name' => $name,
+            )
         );
-
-        return json_decode($response)->contact_id;
-    }
-    
-    /**
-     * Get user infos
-     */
-    public function userGetInfos()
-    {
-        static $apiMethod = 'userInfos';
-
-        $response = $this->execute(
-            $apiMethod,
-            array(),
-            'GET'
-        );
-
-        return json_decode($response)->infos;
-    }
-
-    /**
-     * Get all lists
-     */
-    public function listGetAll()
-    {
-        static $apiMethod = 'listsAll';
-
-        $response = $this->execute(
-            $apiMethod,
-            array(),
-            'GET'
-        );
-
-        return json_decode($response)->lists;
-    }
-    
-    /**
-     * Get domains list
-     */
-    public function userGetDomains()
-    {
-        static $apiMethod = 'userDomainlist';
-
-        $response = $this->execute(
-            $apiMethod,
-            array(),
-            'GET'
-        );
-
-        return json_decode($response)->domains;
-    }
-    
-    /**
-     * Get contact infos
-     */
-    public function contactGetInfos($email)
-    {
-        static $apiMethod = 'contactInfos';
-
-        $response = $this->execute(
-            $apiMethod,
-            array(
-                'contact' => $email,
-            ),
-            'GET'
-        );
-
-        return json_decode($response)->contact;
+        return $response;
     }
 }
